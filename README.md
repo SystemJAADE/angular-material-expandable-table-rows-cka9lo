7cd6d0d34284424ce2c7f850496cc89d34d9e820
SystemJAADE <systemjaade@gmail.com>
2020-10-18 11:56:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-expandable-table-rows-cka9lo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-expandable-table-rows-cka9lo)
---
+++ app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+// Material Modules
+import {
+  MatToolbarModule,
+  MatTableModule,
+} from '@angular/material';
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatTableModule,
+  ],
+  declarations: [AppComponent, TableComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
